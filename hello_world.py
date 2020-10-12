738c7eb413dff4b5ed3acfedc71b8e4869f2a331
Ionut GIRLA <ionut_girla@yahoo.com>
2020-10-12 20:38:04 +0300

    updated hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -5,4 +5,24 @@ Created on Mon Oct 12 20:12:16 2020
 @author: ionut.girla
 """
 
-print("Salut")
+#print("Salut")
+#import matplotlib.pyplot as plt
+#image = plt.imread('path')
+#plt.imshow(image)
+
+
+import cv2
+image = cv2.imread('path')
+cv2.imshow('image',image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#import <opencv/cv.h>
+#import <string>
+
+int main(){
+        std::string s = 'path'
+        cv::Mat im = imread ('path')
+        imshow('image', image);
+        cv:waitKey(0)
+        }
